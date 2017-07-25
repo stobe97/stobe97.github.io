dec104f8f12132e0c1f936670338c0e99996f0a0
stobe97 <30377006+stobe97@users.noreply.github.com>
2017-07-24 22:34:55 -0700

    a

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+window.onload = function() {
+  navigator.getUserMedia = (navigator.getUserMedia ||
+              navigator.webkitGetUserMedia ||
+              navigator.mozGetUserMedia ||
+              navigator.msGetUserMedia);
+}
+
+if(navigator.getUserMedia) {
+  navigator.getUserMedia({audio: true},
+        function(stream) {
+          var microphone = context.createMediaStreamSource(stream);
+          microphone.connect(context.destination);
+        }, errorCallback);
+}
+else {
+  alert('Browser does not support getUserMedia.');
+}
+
+var context = new AudioContext();
 var AudioContext = window.AudioContext || window.webkitAudioContext
 
 module.exports = WebAudioAnalyser
